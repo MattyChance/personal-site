73e22723aa6d4fb40b50c964032cd057cb8ace3e
wangmattychen <chenmatty.wang@gmail.com>
2016-12-02 00:17:40 -0500

    side nav menu pushes the content; added overlay but not transparent enough

--- script/app.js
+++ script/app.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
 	$('.nav-button').click(function() {
 		$('#mySidenav').toggleClass('openSidenav');
-
+		$('#main').toggleClass('pushContent');
+		$('body').toggleClass('overlay');
 		if ($('.nav-button').text() === '+') {
 			$('.nav-button').text('-');
 

848a5deb7c7e033906de6c2420f0a22e37dc6da9
wangmattychen <chenmatty.wang@gmail.com>
2016-12-02 22:22:24 -0500

    added overlay

--- script/app.js
+++ script/app.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 	$('.nav-button').click(function() {
 		$('#mySidenav').toggleClass('openSidenav');
 		$('#main').toggleClass('pushContent');
-		// $('body').toggleClass('overlay');
+		$('body').toggleClass('overlay');
 		if ($('.nav-button').text() === '+') {
 			$('.nav-button').text('-');
 
